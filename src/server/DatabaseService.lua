6264be4b4c924a22af4bd419ed933bdcee0bc468
shiroi_GithubDesktop <86756689+ShiroiKegawa32@users.noreply.github.com>
2022-02-27 19:06:07 +0400

    pub
    i need to do this because my os farted on updating my file system

---
+++ src/server/DatabaseService.lua
@@ -0,0 +1,16 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Libraries = ReplicatedStorage:WaitForChild("Libraries")
+
+local DataStore2 = require(Libraries:WaitForChild("DataStore2"))
+
+DataStore2.Combine("Internal", "InternalSettings")
+
+local DatabaseService = {}
+
+--get settings from database with DataStore2
+function DatabaseService.GetInitializeSettings(Player)
+    local settings = DataStore2("InternalSettings", Player)
+    return settings:Get("InternalSettings")
+end
+
+return DatabaseService
--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,11 @@
-print("Hello world, from server!")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+local PlayerService = require(ServerScriptService.TacticalLiquidServer.PlayerService)
+local DatabaseService = require(ServerScriptService.TacticalLiquidServer.DatabaseService)
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+PlayerService.GetPlayerJoinedEvent():Connect(function(player)
+    -- local InternalSettings = DatabaseService.GetInitializeSettings(player)
+end)
+
