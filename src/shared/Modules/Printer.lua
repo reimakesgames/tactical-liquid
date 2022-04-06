e0d959d85e408d6250e1614fdbecf5e457ecef0a
Synthranger <synthrangerofficial@gmail.com>
2022-04-06 13:05:28 +0800

    Update Printer.lua

--- src/shared/Modules/Printer.lua
+++ src/shared/Modules/Printer.lua
@@ -5,17 +5,17 @@ export type Printer = {
     print: (message: string, severity: string) -> (nil)
 }
 
-function PRINTER.new(name)
-    local _printer: Printer = {
-        name = name;
-        print = function(self, message, severity)
-            severity = severity or "Info"
-            message = message or "No Message Provided"
-            local logMessage = "[ "..severity.." ] [ " .. name .. " ]: " .. message
-            print(logMessage)
-        end
-    }
+local LOG_TEMPLATE = "[ %s ] [ %s ]: %s"
+local format = string.format
 
+function PRINTER:print(message, severity)
+    severity = severity or "Info"
+    message = message or "Mga kuwentong hindi kapakapaniwala"
+    print(format(LOG_TEMPLATE, severity, self.name, message))
+end
+
+function PRINTER.new(name)
+    local _printer: Printer = setmetatable({name = name;}, PRINTER)
     return _printer
 end
 
